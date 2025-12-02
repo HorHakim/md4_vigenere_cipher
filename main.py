19be8ba22e4d518a14284641a4a88410ffd96a52
Hakim <horairyhakim@gmail.com>
2025-12-02 16:22:25 +0100

    feature: cesar uncipher function

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ def cesar_cipher(message, key):
 	return crypted_message
 
 
+def cesar_uncipher(crypted_message, key):
+	return cesar_cipher(crypted_message, -key)
+
+
+
 crypted_message = cesar_cipher(message="lapin", key=3)
 print(crypted_message)
+
+initial_message = cesar_uncipher(crypted_message=crypted_message, key=3)
+print(initial_message)
