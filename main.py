4327df8e932223f2c23793f6940b3590b9e74444
Hakim <horairyhakim@gmail.com>
2025-12-03 12:03:14 +0100

    feature: vigenere cipher function

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def cesar_uncipher(crypted_message, key):
 	return cesar_cipher(crypted_message, -key)
 
 
-
 def brute_force_cesar_cipher(crypted_message):
 	for potential_key in range(1, 1_114_112):
 		potential_message = cesar_uncipher(crypted_message, key=potential_key)
@@ -24,11 +23,31 @@ def brute_force_cesar_cipher(crypted_message):
 			print("----------------")
 
 
+def vigenere_cipher(message, password):
 
-crypted_message = cesar_cipher(message="lapin", key=554)
+	list_of_keys = [ord(char) for char in password]
+	crypted_message = ""
+
+	for index, char in enumerate(message):
+
+		current_key = list_of_keys[index % len(list_of_keys)]
+		crypted_char = cesar_cipher(message=char, key=current_key)
+
+		crypted_message += crypted_char
+
+	return crypted_message
+
+
+
+# crypted_message = cesar_cipher(message="lapin", key=554)
+# print(crypted_message)
+
+# initial_message = cesar_uncipher(crypted_message=crypted_message, key=554)
+# print(initial_message)
+
+# brute_force_cesar_cipher(crypted_message)
+
+
+crypted_message = vigenere_cipher(message="Bonjour, comment ça va ?", password="chocolat123!")
 print(crypted_message)
 
-initial_message = cesar_uncipher(crypted_message=crypted_message, key=554)
-print(initial_message)
-
-brute_force_cesar_cipher(crypted_message)
